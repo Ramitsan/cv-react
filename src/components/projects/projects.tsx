6285400d07fc1e21c8f515f66d4cc6a460339fbd
Ramitsan <ramitsan@gmail.com>
2024-09-29 03:35:17 +0300

    add project sorting

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -5,22 +5,24 @@ import './projects.css';
 import { Filters } from "../filters/filters";
 
 
-export function Projects() { 
+export function Projects() {
   const [filterTitle, setFilterId] = useState('All');
 
   const filteredProjects = useMemo(() => {
     return projects.filter(project => project.technologies.find(tec => filterTitle == 'All' ? true : tec == filterTitle) != null)
   }, [filterTitle]);
-  
+
   return (
     <section className="projects">
-      <h2 className="heading heading--second-level">My projects</h2> 
+      <h2 className="heading heading--second-level">My projects</h2>
 
       <Filters filterTitle={filterTitle} onClickFilter={(id) => {
-            setFilterId(id);
-          }} />     
-      <div className="projects__list">        
-        {filteredProjects.map(project => <Project key={project.id} project={project} />)} 
+        setFilterId(id);
+      }} />
+      <div className="projects__list">
+        {filteredProjects.sort((a, b) => Number(b.year) - Number(a.year))
+          .sort((a, b) => Number(b.id) - Number(a.id))
+          .map(project => <Project key={project.id} project={project} />)}
       </div>
     </section>
   )
